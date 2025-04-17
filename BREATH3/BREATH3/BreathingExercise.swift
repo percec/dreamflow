ac2e37276cd90a99af542624d09708a18258da7b
Tudor Percec <prctudor@gmail.com>
2025-04-17 09:34:11 +0800

    Delete BREATH3 directory

--- BREATH3/BREATH3/BreathingExercise.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct BreathingExercise: Identifiable {
-    let id = UUID()
-    var name: String
-    var description: String
-    var animationSequence: [CGFloat]
-    var phaseDurations: [UInt64]  // in nanoseconds
-}
